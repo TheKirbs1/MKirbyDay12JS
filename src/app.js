dc9ef3030b592010663fced3c24aa7810db6e9dd
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-04-06 00:46:14 +0000

    done in class, finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adjective = ["great", "big", "gnarly", "tiny"];
+  let noun = ["jogger", "raccoon", "driver", "guy"];
+  let URL = [".com", ".fr", ".io", ".net", ".tv"];
+
+  console.log("Regular for Loop");
+  for (let pronounindex = 0; pronounindex < pronoun.length; ++pronounindex) {
+    for (let adjindex = 0; adjindex < adjective.length; ++adjindex) {
+      for (let nounindex = 0; nounindex < noun.length; ++nounindex) {
+        for (let URLindex = 0; URLindex < URL.length; ++URLindex) {
+          console.log(
+            `${pronoun[pronounindex]}${adjective[adjindex]}${noun[nounindex]}${URL[URLindex]}`
+          );
+        }
+      }
+    }
+  }
 };
